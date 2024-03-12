4cb108cd675bc7f06a3755d6c0595adb7dd5afae
Adriana Santiago Morales <adriana.santiago@dgtid.oaxaca.gob.mx>
2024-03-12 16:17:35 -0600

    Uso de Comentarios en GO

---
+++ concepts/comentarios/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Domain es el nombre de dominio de EDteam
+const Domain = "EDteam"
+
+// Main return empty
+//
+// La intención de esta función es ver cómo generar documentación go, quiero ver con
+// mis propios ojos que realmente salen.
+func Main() {
+	fmt.Println("Regresar texto")
+}
