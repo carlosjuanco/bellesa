55ff6dd7abc1400a3fe690885b57020373edb5c5
carlosjuanco <carlosjuancho328@gmail.com>
2024-03-15 00:53:44 -0600

    Establecer el titulo del documento Agregar otro ejemplo del modelo de caja

--- Modelo de caja/main.js
+++ Modelo de caja/main.js
@@ -8,3 +8,27 @@ intrinsicSwitch.addEventListener("change", () => {
     intrinsicSwitch.checked ? "intrinsic" : "extrinsic"
   );
 });
+
+// Segundo ejemplo
+const parentBox = document.querySelector('[data-element="parent-box"]');
+const heightLabel = document.querySelector('[data-element="height-label"]');
+const widthLabel = document.querySelector('[data-element="width-label"]');
+const intrinsicSwitch2 = document.querySelector("#intrinsic-switch2");
+
+// Update the size labels on resize
+const observer = new ResizeObserver((observedItems) => {
+  const { borderBoxSize } = observedItems[0];
+
+  widthLabel.innerText = `${Math.round(borderBoxSize[0].inlineSize)}px`;
+  heightLabel.innerText = `${Math.round(borderBoxSize[0].blockSize)}px`;
+});
+
+observer.observe(parentBox);
+
+// Set sizing attribute based on switch
+intrinsicSwitch2.addEventListener("change", () => {
+  parentBox.setAttribute(
+    "data-sizing",
+    intrinsicSwitch2.checked ? "intrinsic" : "extrinsic"
+  );
+});
