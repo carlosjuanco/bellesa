6e11d3dbbc0be0d3700e7d3c3f9364e163f29fb1
carlosjuanco <carlosjuancho328@gmail.com>
2024-03-16 15:17:08 -0600

    Punteros

---
+++ concepts/3_1_punteros/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Puntero: Variables que almacenan la dirección en memoria de un valor
+	var color string = "🟧"
+	var pointerColor *string
+	pointerColor = &color
+	*pointerColor = "🟦"
+
+	fmt.Printf("Tipo: %T, Valor: %s, Dirección: %v\n", color, color, &color)
+	fmt.Printf("Tipo: %T, Valor: %v\n, Desreferenciación: %s\n",
+		pointerColor, pointerColor, *pointerColor)
+}
