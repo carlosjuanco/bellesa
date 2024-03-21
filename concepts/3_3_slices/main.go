9dd36a6e93ee450b5629d93571a77f8c73f0e115
carlosjuanco <carlosjuancho328@gmail.com>
2024-03-20 19:02:55 -0600

    slices

--- concepts/3_3_slices/main.go
+++ concepts/3_3_slices/main.go
@@ -5,11 +5,23 @@ import "fmt"
 func main() {
 	// slices: Son apuntadores a array, no poseen datos
 	things := [7]string{"🚕", "🚗", "🚘", "🚙", "🚔", "🚨", "🎈"}
-	cars := things[0:5]
+	cars := things[0:5] // "🚕", "🚗", "🚘", "🚙", "🚔"
+	red := things[5:7]  // "🚨", "🎈"
 
-	// El indice final es excluyente, debe de sumarse uno para obtener hasta
+	// El indice final es "Excluyente", debe de sumarse uno para obtener hasta
 	// la posición que se desea.
 
 	fmt.Println("Things: ", things)
 	fmt.Println("Cars: ", cars)
+	fmt.Println("Red: ", red)
+	fmt.Println("Cars[0]: ", cars[0])
+	fmt.Println("Red[0]: ", red[0])
+
+	//Modificar
+	red[1] = "🚑"
+	fmt.Println("Red[1]", red[1])
+	fmt.Println("Things: ", things)
+	fmt.Println("Cars: ", cars)
+	fmt.Println("Red: ", red)
+	fmt.Println("Cars[0]: ", cars[0])
 }
