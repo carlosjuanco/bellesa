b8cf8fb61bac6ce51af3a690d8cefc26211c8d40
carlosjuanco <carlosjuancho328@gmail.com>
2024-04-20 13:42:06 -0600

    Tipos de datos

--- concepts/2_4_tiposdedatos/main.go
+++ concepts/2_4_tiposdedatos/main.go
@@ -6,7 +6,7 @@ func main() {
 	// bool
 	var a bool = true
 	fmt.Printf("Tipo: %T, Valor: %v\n", a, a)
-	
+
 	// string
 	var b string = "juancho"
 	fmt.Printf("Tipo: %T, Valor: %v\n", b, b)
@@ -15,35 +15,34 @@ func main() {
 
 	/*
 		`uint`
-    	* uint8  unsigned 8-bit  integers (0 to 255)
-    	* uint16 unsigned 16-bit integers (0 to 65535)
-    	* uint32 unsigned 32-bit integers (0 to 4294967295)
-    	* uint64 unsigned 64-bit integers (0 to 18446744073709551615)
+		* uint8  unsigned 8-bit  integers (0 to 255)
+		* uint16 unsigned 16-bit integers (0 to 65535)
+		* uint32 unsigned 32-bit integers (0 to 4294967295)
+		* uint64 unsigned 64-bit integers (0 to 18446744073709551615)
 
 
-    	`uint`
-   	 * int8  signed 8-bit  integers (-128 to 127)
-    	* int16 signed 16-bit integers (-32768 to 32767)
-    	* int32 signed 32-bit integers (-2147483648 to 2147483647)
-    	* int64 signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+		`uint`
+		* int8  signed 8-bit  integers (-128 to 127)
+		* int16 signed 16-bit integers (-32768 to 32767)
+		* int32 signed 32-bit integers (-2147483648 to 2147483647)
+		* int64 signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
 
 
-    	`byte` // alias for uint8
+		`byte` // alias for uint8
 
 
-    	`rune` // alias for int32
-           // represents a Unicode code point
+		`rune` // alias for int32
+			// represents a Unicode code point
 
 
-    	`float32` `float64`
+		`float32` `float64`
 	*/
 
 	// Restricciones entre tipos de datos numéricos
-	// No se pueden hacer operaciones entre los diferentes tipos de datos enteros 
+	// No se pueden hacer operaciones entre los diferentes tipos de datos enteros
 	// y con punto flotante.
 	var aa uint8 = 200
 	var bb uint16 = 800
-
 
 	// c := aa + bb
 	// fmt.Printf("Tipo: %T, Valor: %v", c, c)
@@ -62,35 +61,35 @@ func main() {
 	fmt.Printf("Tipo: %T, Valor: %v\n", d, d)
 	fmt.Printf("Ignorar el índice del ciclo for\n")
 	numeros := []int{1, 2, 3, 4, 5}
-    for _, valor := range numeros {
-        fmt.Println(valor)
-    }
-    fmt.Printf("¿Que pasa si solo dejamos la variable valor\n")
-    for valor := range numeros {
-        fmt.Println(valor)
-    }
-    fmt.Printf("Ignorar valores de retorno de una función:\n")
+	for _, valor := range numeros {
+		fmt.Println(valor)
+	}
+	fmt.Printf("¿Qué pasa si solo dejamos la variable valor?\n")
+	for valor := range numeros {
+		fmt.Println(valor)
+	}
+	fmt.Println("En realidad se imprime el índice")
 
-    // Ignora el primer valor retornado
-    _, segundoValor := retornaDosValores()
-    fmt.Println(segundoValor)
+	fmt.Printf("Ignorar valores de retorno de una función:\n")
+	// Ignora el primer valor retornado
+	_, segundoValor := retornaDosValores()
+	fmt.Println(segundoValor)
 
-    fmt.Printf("Ignorar valores en la declaración de asignación:\n")
-    var z, _ int = 5, 10
-    fmt.Println(z) // Imprime: 5
+	fmt.Printf("Ignorar valores en la declaración de asignación:\n")
+	var z, _ int = 5, 10
+	fmt.Println(z) // Imprime: 5
 
-    fmt.Printf("Pregunte en ChatGPT 3.5 y me quede con la misma duda\n")
-    
+	fmt.Printf("Pregunte en ChatGPT 3.5 y me quede con la misma duda\n")
 
 	fmt.Printf("Valor por defecto de los tipo de datos\n")
 	fmt.Printf("Bool\n")
 	var g bool
 	fmt.Printf("Tipo: %T, Valor: %v\n", g, g)
-	
+
 	fmt.Printf("string\n")
 	var h string
 	fmt.Printf("Tipo: %T, Valor: %v\n", h, h)
-	
+
 	fmt.Printf("Numérico\n")
 	var i uint8
 	fmt.Printf("Tipo: %T, Valor: %v\n", i, i)
