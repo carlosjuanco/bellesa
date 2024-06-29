96b5b2c22c0893db0792b2c3983e7d7625129e60
carlosjuanco <carlosjuancho328@gmail.com>
2024-06-29 00:29:44 -0600

    Leccion 3

--- Lecsion2/sandbox.js
+++ Lecsion2/sandbox.js
@@ -1,6 +1,14 @@
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+// character = 20
+character = 'luigui';
+// age = 'yoshi'
+age = 40;
+// isBlackBelt = 'yes'
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circ('hello'))
+console.log(circ(7.5));
