fbc162a244cfc3d57b40969c071b22dcddad0807
carlosjuanco <carlosjuancho328@gmail.com>
2024-10-22 13:16:55 -0600

    Proyecto iasd debian 12.7

---
+++ debian_setup_api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="https://github.com/carlosjuanco/zeus-api.git"
+DEST_DIR="/home/juan/Documentos/proyecto_iasd/api"
+
+# Comando para clonar el repositorio
+git clone $REPO_URL $DEST_DIR
+
+# Mensaje de confirmación
+echo "Repositorio clonado en $DEST_DIR"
