122f09953a93844013cc7346828c762ec018ea1d
carlosjuanco <carlosjuancho328@gmail.com>
2024-10-24 04:01:20 -0600

    Proyecto iasd debian 12.7

--- debian_setup_api.sh
+++ debian_setup_api.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# Variables
-REPO_URL="https://github.com/carlosjuanco/zeus-api.git"
-DEST_DIR="/home/juan/Documentos/proyecto_iasd/api/zeus-api"
+# Variables para repositorio API
+REPO_API_URL="https://github.com/carlosjuanco/zeus-api.git"
+DEST_API_DIR="/home/juan/Documentos/proyecto_iasd/api/zeus-api"
 
 # Comando para clonar el repositorio
-git clone $REPO_URL $DEST_DIR
+git clone $REPO_API_URL $DEST_API_DIR
 
 # Mensaje de confirmación
-echo "Repositorio clonado en $DEST_DIR"
+echo "Repositorio clonado en $DEST_API_DIR"
+
+# Variables para repositorio APP
+REPO_APP_URL="https://github.com/carlosjuanco/meca-app.git"
+DEST_APP_DIR="/home/juan/Documentos/proyecto_iasd/app/meca-app"
+
+# Comando para clonar el repositorio
+git clone $REPO_APP_URL $DEST_APP_DIR
+
+# Mensaje de confirmación
+echo "Repositorio clonado en $DEST_APP_DIR"
