34fa5cb76d3d999da16ff1d429365e7e08ca1b2a
carlosjuanco <carlosjuancho328@gmail.com>
2024-10-26 11:07:56 -0600

    Proyecto iasd debian 12.7

--- debian_setup_api.sh
+++ debian_setup_api.sh
@@ -19,3 +19,28 @@ git clone $REPO_APP_URL $DEST_APP_DIR
 
 # Mensaje de confirmación
 echo "Repositorio clonado en $DEST_APP_DIR"
+
+input="/home/juan/Documentos/proyecto_iasd/api/zeus-api/.env.example"
+out="/home/juan/Documentos/proyecto_iasd/api/zeus-api/.env"
+touch $out
+db_host="DB_HOST=127.0.0.1"
+db_database="DB_DATABASE=laravel"
+db_password="DB_PASSWORD="
+
+while read linea
+do
+	# echo $linea
+	if [ -z "$linea" ]; then
+		echo $linea >> $out
+	else
+		if [ $linea = $db_host ]; then
+	  		echo "DB_HOST=192.168.20.10" >> $out
+	  	elif [ $linea = $db_database ]; then
+	  		echo "DB_DATABASE=iasd" >> $out
+	  	elif [ $linea = $db_password ]; then
+	  		echo "DB_PASSWORD=juan" >> $out
+		else
+			echo $linea >> $out
+		fi
+	fi
+done < $input
