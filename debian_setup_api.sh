8fb393e255cf912bc623766c8472276f26a49424
carlosjuanco <carlosjuancho328@gmail.com>
2024-10-30 00:36:42 -0600

    Proyecto iasd debian 12.7 Mostrar los mensajes de permisos que deben ser iguales a los que copie cuando todavia realizaba las carpetas desde el modo grafico

--- debian_setup_api.sh
+++ debian_setup_api.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+echo "Sistema operativo: Linux"
+echo "Distribución: Debian"
+echo "Versión: 12.7"
+echo "Fecha: 30 de Octubre del 2024"
+echo "Proyecto: iasd"
+echo "Descripción del proyecto: Proyecto iglesia adventista del séptimo día"
+echo "......................................................................"
+
 directorio_carpeta_raiz="/home/juan/Documentos"
 
 carpeta_raiz="/home/juan/Documentos/proyecto_iasd"
@@ -10,7 +19,7 @@ carpeta_repositorio_api="/home/juan/Documentos/proyecto_iasd/api/zeus-api"
 carpeta_repositorio_app="/home/juan/Documentos/proyecto_iasd/app/meca-app"
 
 # Paramos todos los contenedores
-echo "Paramos todos los contenedores ...."
+echo "Comenzando a parar todos los contenedores ...."
 
 sudo docker stop iasd_bd
 sudo docker stop iasd_app
@@ -18,9 +27,11 @@ sudo docker stop instalar_dependencias_en_api
 sudo docker stop instalar_dependencias_en_app
 sudo docker stop iasd_api
 
-echo "Se pararon todos los contenedores"
+echo "Se termino de parar todos los contenedores"
 
-echo "Eliminar todos los contenedores, para que no haya problemas al momento de crear las carpetas ...."
+echo "......................................................................"
+
+echo "Comenzar a eliminar todos los contenedores ...."
 
 sudo docker rm iasd_bd
 sudo docker rm iasd_app
@@ -28,16 +39,20 @@ sudo docker rm iasd_api
 sudo docker rm instalar_dependencias_en_api
 sudo docker rm instalar_dependencias_en_app
 
-echo "Se eliminaron todos los contenedores"
+echo "Se termino de eliminar todos los contenedores"
 
-echo "Verificamos que la carpeta proyecto_iasd no exista, si existe lo eliminamos"
+echo "......................................................................"
+
+echo "Verificando que la carpeta proyecto_iasd no exista ...."
 
 if [ -d "$carpeta_raiz" ]; then
-	echo "El archivo existe $carpeta_raiz, procedemos a borrar ...."
+	echo "La carpeta exite $carpeta_raiz, comenzando a borrar ...."
 	sudo rm -r $carpeta_raiz
 fi
 
-echo "Creando repositorio ....."
+echo "......................................................................"
+
+echo "Comenzando a crear las carpetas ...."
 
 mkdir $carpeta_raiz
 mkdir $subcarpeta_bd
@@ -46,15 +61,34 @@ mkdir $subcarpeta_app
 mkdir $carpeta_repositorio_api
 mkdir $carpeta_repositorio_app
 
-echo "Se crearon las carpetas"
+echo "Se termino de crear las carpetas"
 
-echo "Permisos de las carpetas"
+tree $carpeta_raiz
+
+echo "......................................................................"
+
+echo "Estos eran los permisos cuando creaba las carpetas en el explorador de archivo ...."
+
+echo "drwxr-xr-x  5 juan juan 4096 oct 26 11:02 proyecto_iasd"
+echo "drwxr-xr-x 3 juan juan 4096 oct 26 11:02 api"
+echo "drwxr-xr-x 3 juan juan 4096 oct 26 11:02 app"
+echo "drwxr-xr-x 8  999 juan 4096 oct 26 11:07 bd"
+echo "drwxr-xr-x 14 juan juan 4096 oct 26 11:03 zeus-api"
+echo "drwxr-xr-x 6 juan juan 4096 oct 26 11:04 meca-app"
+echo "Lo importante es drwxr-xr-x"
+
+echo "......................................................................"
+
+echo "Los permisos de las carpetas creadas son ...."
 ls -al $directorio_carpeta_raiz
 ls -al $carpeta_raiz
 ls -al $subcarpeta_api
 ls -al $subcarpeta_app
 
-Variables para repositorio API
+echo "......................................................................"
+echo "Comenzando a clonar los repositorios ...."
+
+# Variables para repositorio API
 REPO_API_URL="https://github.com/carlosjuanco/zeus-api.git"
 DEST_API_DIR="/home/juan/Documentos/proyecto_iasd/api/zeus-api"
 
@@ -73,6 +107,9 @@ git clone $REPO_APP_URL $DEST_APP_DIR
 
 # Mensaje de confirmación
 echo "Repositorio clonado en $DEST_APP_DIR"
+
+echo "......................................................................"
+echo "Comenzando a crear el archivo .env en zeus-api ...."
 
 input="/home/juan/Documentos/proyecto_iasd/api/zeus-api/.env.example"
 out="/home/juan/Documentos/proyecto_iasd/api/zeus-api/.env"
@@ -98,3 +135,5 @@ do
 		fi
 	fi
 done < $input
+
+echo "Se termino de crear el archivo .env en zeus-api"
