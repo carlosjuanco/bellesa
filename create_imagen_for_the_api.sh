99a07460b899db2b3681163e87af736033eff546
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-15 16:03:06 -0600

    AED-13 En debian filament Automatizar la creacion de imagen

--- create_imagen_for_the_api.sh
+++ create_imagen_for_the_api.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-version_so="Versión: Sonoma 14.3"
-proyecto="Proyecto: main"
-descripcion_proyecto="Descripción del proyecto: Base para todos los proyectos."
+name_project="image_for_filament"
+
+version_so="Versión: Debian 12.7"
+proyecto="Proyecto: "$name_project
+descripcion_proyecto="Descripción del proyecto: Base de filament para los siguientes proyecto."
 
 echo $version_so
 echo $proyecto
@@ -10,21 +12,24 @@ echo $descripcion_proyecto
 echo "......................................................................"
 
 current_username=$(whoami)
-
-directorio_carpeta_raiz="/Users/$current_username/Documents"
-
-carpeta_raiz="/Users/$current_username/Documents/proyecto_main"
-subcarpeta_api="/Users/$current_username/Documents/proyecto_main/api"
-
 current_directory_of_the_bellesa_project=$(pwd)
+
+directory_name_project="proyecto_"$name_project
+directorio_carpeta_raiz="/home/$current_username/Documentos"
+carpeta_raiz=$directorio_carpeta_raiz"/"$directory_name_project
+subcarpeta_api=$carpeta_raiz"/api"
+
+container_name_for_filament_image=$name_project
+
+create_container_for_services="create_containers_for_"$name_project"_services.yml"
 
 # Paramos todos los contenedores
 echo "Comenzando a parar todos los contenedores ...."
 
-sudo docker stop proyecto_api2
+sudo docker stop $container_name_for_filament_image
 if [ $? -ne 0 ]; then
     echo ".....El Error response from daemon: No such container"
-    echo ".....Se debe a que no existe el contenedor iasd_bd"
+    echo ".....Se debe a que no existe el contenedor "$container_name_for_filament_image
 fi
 
 echo "Se termino de parar todos los contenedores"
@@ -33,16 +38,16 @@ echo "......................................................................"
 
 echo "Comenzar a eliminar todos los contenedores ...."
 
-sudo docker rm proyecto_api2
+sudo docker rm $container_name_for_filament_image
 if [ $? -ne 0 ]; then
     echo ".....El Error response from daemon: No such container"
-    echo ".....Se debe a que no existe el contenedor iasd_bd"
+    echo ".....Se debe a que no existe el contenedor "$container_name_for_filament_image
 fi
 echo "Se termino de eliminar todos los contenedores"
 
 echo "......................................................................"
 
-echo "Verificando que la carpeta proyecto_main no exista ...."
+echo "Verificando que la carpeta "$directory_name_project" no exista ...."
 
 if [ -d "$carpeta_raiz" ]; then
 	echo "La carpeta exite $carpeta_raiz, comenzando a borrar ...."
@@ -64,8 +69,8 @@ echo "......................................................................"
 
 echo "Estos eran los permisos cuando creaba las carpetas en el explorador de archivo ...."
 
-echo "drwxr-xr-x  5 juan juan 4096 oct 26 11:02 proyecto_main"
-echo "drwxr-xr-x 3 juan juan 4096 oct 26 11:02 api"
+echo "drwxr-xr-x  5 juan juan 4096 oct 26 11:02 "$directory_name_project
+echo "drwxr-xr-x 3 juan juan 4096 oct 26 11:02 "$subcarpeta_api
 echo "Lo importante es drwxr-xr-x"
 
 echo "......................................................................"
@@ -76,38 +81,29 @@ ls -al $carpeta_raiz
 ls -al $subcarpeta_api
 
 echo "......................................................................"
-# Objetivo: copiar el contenido del archivo (automate_image_for_api.yml) y 
-# reemplazar una cadena específica ("/home/juan/Documentos/") con otra ("/Users/$current_username/Documents/") 
-# luego escribir el resultado en un nuevo archivo (create_imagen_for_the_api2.yml).
-
-# El comando "sed" utiliza la opción -i para editar el archivo de destino 
-# en lugar de imprimir el resultado en la consola.
-
-# La expresión regular s|/home/juan/Documentos/|/Users/$current_username/Documents/|g reemplaza la cadena "/home/juan/Documentos/" 
-# con "/Users/$current_username/Documents/" de manera global (g) en el archivo de destino.
-
-echo "Comenzando a crear el archivo create_imagen_for_the_api2.yml ...."
+echo "Comenzando a crear el archivo "$create_container_for_services" .v..."
 echo "......................................................................"
 origen="$current_directory_of_the_bellesa_project/automate_image_for_api.yml"
-destino="$current_directory_of_the_bellesa_project/create_imagen_for_the_api2.yml"
+destino="$current_directory_of_the_bellesa_project/"$create_container_for_services
 
-# Copiar el contenido del archivo de origen al archivo de destino
+#v Copiar el contenido del archivo de origen al archivo de destino
 cp "$origen" "$destino"
 
-# Reemplazar la cadena en el archivo de destino
-sed -i '' "s|/home/juan/Documentos|/Users/$current_username/Documents|g" "$destino"
+sed -i "s|proyecto_api:|"$name_project":|g" "$destino"
+sed -i "s|container_name: proyecto_api2|container_name: "$container_name_for_filament_image"|g" "$destino"
+sed -i "s|/home/juan/Documentos/proyecto_main|"$carpeta_raiz"|g" "$destino"
 
-echo "Se termino de crear el archivo create_imagen_for_the_api2.yml"
+echo "Se termino de crear el archivo "$create_container_for_services
 echo "......................................................................"
 
 echo "Comenzando a crear la imagen para la API ...."
-docker-compose -f create_imagen_for_the_api2.yml up -d
+sudo docker-compose -f $create_container_for_services up -d
 
-docker logs -f proyecto_api2
+sudo docker logs -f $container_name_for_filament_image
 
 echo "......................................................................"
-echo "Se termino de levantar el servicio proyecto_api2, sí muestra el siguiente mensaje"
+echo "Se termino de levantar el servicio "$container_name_for_filament_image", sí muestra el siguiente mensaje"
 echo "Setting up zip (3.0-13)"
 echo "......................................................................"
-echo "Eliminar el archivo create_imagen_for_the_api2.yml ...."
-rm create_imagen_for_the_api2.yml
+echo "Eliminar el archivo $create_container_for_services ...."
+sudo rm $create_container_for_services
