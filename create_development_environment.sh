51c271a151f3fd8ebe1fa3e6855ede7337a9f3ff
carlosjuanco <carlosjuancho328@gmail.com>
2025-06-03 14:03:29 -0600

    AED-14 En macOs SDAC

--- create_development_environment.sh
+++ create_development_environment.sh
@@ -30,6 +30,7 @@ container_name_install_dev_on_api=$name_project"_instalar_dependencias_en_api"
 container_name_install_dev_on_app=$name_project"_instalar_dependencias_en_app"
 
 docker_image_name_container_api="juancholll/laravel_api_macos"
+api_port_number=8082
 
 # Paramos todos los contenedores
 echo "Comenzando a parar todos los contenedores ...."
@@ -231,7 +232,7 @@ out=$carpeta_repositorio_app"/.env"
 cp "$input" "$out"
 
 # Reemplazar la cadena en el archivo de destino
-sed -i '' "s|8081|8082|g" "$out"
+sed -i '' "s|8081|"$api_port_number"|g" "$out"
 
 echo "Se termino de crear el archivo .env en zeus-api"
 
@@ -280,21 +281,14 @@ sed -i '' "s|container_name: iasd_api|container_name: "$container_name_api"|g" "
 sed -i '' "s|/home/juan/Documentos/proyecto_iasd|"$carpeta_raiz"|g" "$destino"
 sed -i '' "s|container_name: iasd_app|container_name: "$container_name_app"|g" "$destino"
 sed -i '' "s|ipv4_address: 192.168.20.12|ipv4_address: 192.168.20.18|g" "$destino"
-sed -i '' "s|8080:80|8082:82|g" "$destino"
+sed -i '' "s|8080:80|"$api_port_number":82|g" "$destino"
 sed -i '' "s|--host=192.168.20.12 --port=80|--host=192.168.20.18 --port=82|g" "$destino"
 sed -i '' "s|ipv4_address: 192.168.20.14|ipv4_address: 192.168.20.19|g" "$destino"
 sed -i '' "s|8081:81|8083:83|g" "$destino"
 sed -i '' "s|npm run serve -- --port 81|npm run serve -- --port 83|g" "$destino"
 
 echo "Se termino de crear el archivo run_services2.yml"
-echo "......................................................................"
-# Objetivo: cambiar el valor de baseURL para que apunte al puerto 8082
-echo "Comenzando ...."
-echo "..................|...................................................."
-destino=$carpeta_repositorio_app"/src/main.ts"
 
-# Reemplazar la cadena en el archivo de destino
-sed -i '' "s|localhost:8080/api|localhost:8082/api|g" "$destino"
 echo "......................................................................"
 # Objetivo: copiar cambiar el nombre de la base de datos (init.sql) y 
 echo "Comenzando a modificar nombre de la base de datos init.sql ...."
