e2d1789012e107896d7de982d997552bd49bb649
carlosjuanco <carlosjuancho328@gmail.com>
2026-07-02 02:23:45 -0600

    AED-34 Refactorizar todo

--- set-project-configuration.sh
+++ set-project-configuration.sh
@@ -641,7 +641,7 @@ deploy_bash_file() {
 
             # PASO 1: Tu terminal (el host)
             run_sed "$CREATE_A_DEVELOPMENT_ENVIRONMENT" \
-                -e "s|{{ EXPORTAR_VARIABLE_PARA_DEFINIR_USUARIO }}|export UID=$(id -u) GID=$(id -g)|g" \
+                -e "s|{{ ENVIRONMENT }}|$env_type|g" \
                 -e "s|sudo docker|docker|g" \
 
             print_success "Archivo creado: $CREATE_A_DEVELOPMENT_ENVIRONMENT"
